2eef1cdfe7ad761b3d212f85f667edc8f0de3468
Mathias Söderberg <mths@sdrbrg.se>
2025-06-02 14:57:44 +0100

    wip

--- layouts/community/60_ansi/mthssdrbrg/keymap.c
+++ layouts/community/60_ansi/mthssdrbrg/keymap.c
@@ -3,8 +3,8 @@
 #define _DEF 0
 #define _FN  1
 
-#define KC_WDLF LCTL(KC_LEFT)
-#define KC_WDRT LCTL(KC_RIGHT)
+#define KC_WDLF LOPT(KC_LEFT)
+#define KC_WDRT LOPT(KC_RIGHT)
 #define CTL_ESC CTL_T(KC_ESC)
 #define FN_ESC LT(_FN, KC_ESC)
 
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEF] = LAYOUT_60_ansi(
             QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_MINS, KC_EQL,  KC_BSPC, \
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_LBRC, KC_RBRC, KC_BSLS, \
-            FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_COLON,  KC_QUOT,          KC_ENT,  \
+            FN_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,          KC_ENT,  \
             SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                     SC_RSPC, \
             KC_LCTL, KC_LWIN, KC_LALT,                           KC_SPACE,                            TG(_FN),   KC_RALT, KC_RWIN, KC_RCTL),
 
